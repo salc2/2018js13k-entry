b3f163aaf0f3426891e7bd05bd706dfc6f3ba6df
salc2 <javiersalcedo75@gmail.com>
2018-09-05 18:39:49 +0100

    map encoded

--- src/sounds.ts
+++
@@ -1,7 +0,0 @@
-import './lib/jsfxr.min';
-
-declare var jsfxr:any;
-
- const soundURL = jsfxr([1,,0.3512,,0.2113,0.3175,,0.1029,,,,,,0.4122,,,,,0.6827,,,,,0.37]); 
- export const jump = new Audio();
- jump.src = soundURL;
